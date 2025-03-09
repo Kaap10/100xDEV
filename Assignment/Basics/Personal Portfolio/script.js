8b25cf1c9189b42623eff18c83b24f5f86b568d2
Vardhman Gupta <vardhmangupta2004@gmail.com>
2025-03-09 23:48:57 +0530

    Complete task: Portfolio

---
+++ Assignment/Basics/Personal Portfolio/script.js
@@ -0,0 +1,5 @@
+// Contact Form Submission Alert
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Message Sent Successfully!");
+});
