b237275405a85d688cea5a6f31aa613a89f0d3c0
Vardhman Gupta <vardhmangupta2004@gmail.com>
2025-03-14 21:49:05 +0530

    Practice: hands-on Js

---
+++ Hands-On/script.js
@@ -0,0 +1,42 @@
+// let person = [
+//     {name: "Alice"},
+//     {roll_no: 43}
+// ];
+
+// let GV = "I am global value: ";
+
+// function myFunction() {
+//     let LV = "I am local value: ";
+//     console.log(GV);
+// }
+
+// console.log(LV);
+
+// let fruits = "apple";
+
+// switch (fruits) {
+//     case "apple":
+//         console.log("It's a Apple!");
+//         break;
+    
+//     case "banana":
+//         console.log("It's a Banana");
+//         break;
+// }
+
+
+// let age = 16;
+// let result = (age>=18) ? "Adult" : "Minor";
+// console.log(result);
+
+// const fName = (params) => {
+//     return result;
+// };
+
+let num = 4;
+
+const fname = (num) => {
+    return num * num;
+};
+
+console.log(fname(num));
